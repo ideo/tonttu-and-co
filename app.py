43b4aefd304c44bc2b5c89d2f3ff4a7dc29bd8e7
Joseph Gambino <joe@gam.bingo>
2020-10-30 13:43:43 +0900

    text is now visible but I don't know why and that's always a good feeling

--- app.py
+++ app.py
@@ -9,19 +9,9 @@ st.title("Tonttu & Co. is here to help!")
 df = load_data()
 st.dataframe(df)
 
-
-# observers = observable("County Brush", 
-#     notebook="d/4f9aa5feff9761c9",
-#     targets=["viewof countyCodes"], 
-#     observe=["selectedCounties"]
-# )
-
-# selectedCounties = observers.get("selectedCounties")
-
-
 observers = observable("Force Graph",
     # notebook="@d3/force-directed-graph",
-    notebook="d/82546d267834ec0a",
+    notebook="@gambingo/force-directed-graph",
     targets=["chart"],
     observe=["data"])
 
